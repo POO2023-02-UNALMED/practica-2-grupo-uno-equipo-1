fb9ef8b5d88bc49022d0f2e379b0bc319a0c303f
SamuelOrtizT <saortizt@unal.edu.co>
2023-11-06 13:23:21 -0500

    creando clases

---
+++ src/gestorAplicacion/Personas/persona.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+
+class Persona(ABC):
+	def __init__(self, nombre="", cedula=000000):
+        self.nombre = nombre
+        self.cedula = cedula
+    def getNombre(self):
+		return self.nombre
+	def getCedula(self):
+		return self.cedula
+	def setNombre(self, nombre):
+		self.nombre = nombre
+	def setCedula(self, cedula):
+		self.cedula = cedula
+	@abstractmethod
+	def puntuacion(self):
+        pass
