04ce463c12497f4956bd88573b5a2a836e63bda5
SamuelOrtizT <saortizt@unal.edu.co>
2023-11-16 19:56:21 -0500

    main en src

--- src/main.py
+++ src/main.py
@@ -8,13 +8,77 @@ from gestorAplicacion.Personas.cocinero import Cocinero
 from gestorAplicacion.Personas.domiciliario import Domiciliario
 from gestorAplicacion.Personas.mesero import Mesero
 from gestorAplicacion.Restaurante.financia import Financia
-from gestorAplicacion.Restaurante.material import Material
+from gestorAplicacion.Restaurante.material import Material, Tipo
 from gestorAplicacion.Restaurante.mesa import Mesa
 from gestorAplicacion.Restaurante.pedido import Pedido
 from gestorAplicacion.Restaurante.plato import Plato
 from gestorAplicacion.Restaurante.reserva import Reserva
 from gestorAplicacion.Restaurante.restaurante import Restaurante
 from gestorAplicacion.Restaurante.turno import Turno, TipoTurno
+
+restaurante = Restaurante()
+
+#Crear un objeto Financia
+financia = Financia(restaurante)
+res = Material(Tipo.RES.value, 100, 100)
+especias = Material(Tipo.ESPECIAS.value, 100, 50)
+aceites = Material(Tipo.ACEITES.value, 100, 100)
+pollos = Material(Tipo.POLLOS.value, 100, 200)
+vinos = Material(Tipo.VINOS.value, 100, 300)
+cebollas = Material(Tipo.CEBOLLAS.value, 100, 50)
+champinones = Material(Tipo.CHAMPINONES.value, 500, 100)
+ajos = Material(Tipo.AJOS.value, 100, 30)
+tomates = Material(Tipo.TOMATES.value, 400, 200)
+quesos = Material(Tipo.QUESOS.value, 300, 150)
+cerdos = Material(Tipo.CERDOS.value, 100, 200)
+atun = Material(Tipo.ATUN.value, 100, 250)
+panes = Material(Tipo.PANES.value, 200, 50)
+pescados = Material(Tipo.PESCADOS.value ,200 ,300 )
+papas = Material(Tipo.PAPAS.value ,200 ,100 )
+huevos = Material(Tipo.PAPAS.value ,200 ,100 )
+
+#Crear turnos
+turno1 = Turno(TipoTurno.SEMANA.value, 2.0, 50000)
+turno2 = Turno(TipoTurno.SEMANA.value, 8.0, 60000)
+turno3 = Turno(TipoTurno.SEMANA.value, 2.0, 70000)
+turno4 = Turno(TipoTurno.DOMINGO.value, 8.0, 55000)
+turno5 = Turno(TipoTurno.SEMANA.value, 2.0, 65000)
+turno6 = Turno(TipoTurno.SEMANA.value, 8.0, 75000)
+turno7 = Turno(TipoTurno.SEMANA.value, 2.0, 60000)
+turno8 = Turno(TipoTurno.SEMANA.value, 8.0, 70000)
+turno9 = Turno(TipoTurno.SEMANA.value, 2.0, 80000)
+turno10 = Turno(TipoTurno.SEMANA.value, 8.0, 65000)
+turno11 = Turno(TipoTurno.SEMANA.value, 3.0, 80000)
+turno12 = Turno(TipoTurno.SABADO.value, 7.0, 50000)
+turno13 = Turno(TipoTurno.SABADO.value, 3.0, 60000)
+turno14 = Turno(TipoTurno.SEMANA.value, 7.0, 70000)
+turno15 = Turno(TipoTurno.SEMANA.value, 2.0, 55000)
+turno16 = Turno(TipoTurno.DOMINGO.value, 3.0, 55000)
+turno17 = Turno(TipoTurno.SABADO.value, 2.0, 55000)
+#Crear empleados
+empleado1 = Mesero("Juan", 123456789, "mesero", restaurante, turno1)
+empleado1.agregarTurno(turno2)
+empleado2 = Cocinero("Fernando", 234567891, "cocinero", restaurante, turno2)
+empleado2.agregarTurno(turno3)
+empleado3 = Domiciliario("Santiago", 345678912, "domiciliario", restaurante, turno4)
+empleado1.agregarTurno(turno5)
+empleado4 = Mesero("Jhon", 123456789, "mesero", restaurante, turno6)
+empleado1.agregarTurno(turno7)
+empleado5 = Cocinero("Moises", 234567891, "cocinero", restaurante, turno8)
+empleado2.agregarTurno(turno9)
+empleado6 = Domiciliario("Rigo", 345678912, "domiciliario", restaurante, turno10)
+empleado1.agregarTurno(turno11)
+empleado7 = Mesero("Brayan", 123456789, "mesero", restaurante, turno12)
+empleado1.agregarTurno(turno13)
+empleado8 = Cocinero("Felipe", 234567891, "cocinero", restaurante, turno14)
+empleado2.agregarTurno(turno15)
+empleado9 = Domiciliario("Martin", 345678912, "domiciliario", restaurante, turno16)
+empleado1.agregarTurno(turno17)
+pedido = Pedido()
+plato = Plato()
+for mesa1 in restaurante.getMesas():
+    mesa1.anadirNumero(mesa1.getNumeroMesa())
+restaurante.borrarReservasViejas()
 
 # funciones ventana de inicio
 def salir():
